ca4ebaabab23810cfa7074d5883da02b5c2f8b06
Pedro <pedrohenriquemottabristot@gmail.com>
2025-02-18 20:51:54 -0300

    Initial commit

---
+++ src/main/java/com/example/ecommerceSimples/EcommerceSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSimplesApplication.class, args);
+	}
+
+}
