7071659a7972cde556b82461b4a4bc986beaefe6
Pedro <pedrohenriquemottabristot@gmail.com>
2025-03-10 19:50:19 -0300

    Add UsuarioController.java

--- src/main/java/com/example/ecommerceSimples/controllers/UsuarioController.java
+++ src/main/java/com/example/ecommerceSimples/controllers/UsuarioController.java
@@ -1,4 +1,46 @@
 package com.example.ecommerceSimples.controllers;
 
+import com.example.ecommerceSimples.dtos.UsuarioDTO;
+import com.example.ecommerceSimples.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UsuarioDTO> findById(@PathVariable Long id) {
+        UsuarioDTO dto = usuarioService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> findAll() {
+        List<UsuarioDTO> dtoList = usuarioService.findAll();
+        return ResponseEntity.ok(dtoList);
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> create(@RequestBody UsuarioDTO dto) {
+        UsuarioDTO createdDto = usuarioService.create(dto);
+        return ResponseEntity.ok(createdDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UsuarioDTO> update(@PathVariable Long id, @RequestBody UsuarioDTO dto) {
+        UsuarioDTO updatedDto = usuarioService.update(id, dto);
+        return ResponseEntity.ok(updatedDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        usuarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
