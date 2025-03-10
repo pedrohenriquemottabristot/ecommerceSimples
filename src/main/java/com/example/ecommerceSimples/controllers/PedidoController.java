f8c8e56bf149cf337b8e0ad8a963e6e56117f7ea
Pedro <pedrohenriquemottabristot@gmail.com>
2025-03-10 19:42:19 -0300

    Add PedidoController.java

--- src/main/java/com/example/ecommerceSimples/controllers/PedidoController.java
+++ src/main/java/com/example/ecommerceSimples/controllers/PedidoController.java
@@ -1,4 +1,47 @@
 package com.example.ecommerceSimples.controllers;
 
+import com.example.ecommerceSimples.dtos.PedidoDTO;
+import com.example.ecommerceSimples.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/pedidos")
 public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PedidoDTO> findById(@PathVariable Long id) {
+        PedidoDTO dto = pedidoService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PedidoDTO>> findAll() {
+        List<PedidoDTO> dtoList = pedidoService.findAll();
+        return ResponseEntity.ok(dtoList);
+    }
+
+    @PostMapping
+    public ResponseEntity<PedidoDTO> create(@RequestBody PedidoDTO dto) {
+        PedidoDTO createdDto = pedidoService.create(dto);
+        return ResponseEntity.ok(createdDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PedidoDTO> update(@PathVariable Long id, @RequestBody PedidoDTO dto) {
+        PedidoDTO updatedDto = pedidoService.update(id, dto);
+        return ResponseEntity.ok(updatedDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        pedidoService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
